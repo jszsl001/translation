bac1ed35e915610506ebd6696301d475e1366ec9
zsl <jszsl001@163.com>
2024-04-15 13:49:37 +0800

    修改配置文件

--- demo.php
+++ demo.php
@@ -1,9 +1,7 @@
 <?php
 require_once "vendor/autoload.php";
 
-
 use jszsl001\GoogleTranslation\Translation;
-
 
 $text = <<<EOT
     The world is a vast and beautiful place, full of wonder and excitement. There are so many things to see and do, and so many people to meet. We can travel to different countries, learn new languages, and experience different cultures. We can try new foods, meet new people, and make new friends. We can learn new things, expand our horizons, and challenge ourselves. The possibilities are endless.
@@ -13,8 +11,10 @@ $text = <<<EOT
 
 $translation = new Translation();
 $result = $translation
-    -> setProxy('http://127.0.0.1:1081')
-    -> setText($text)
+    -> setText($text) // 待翻译的文本
+    -> setSourceLanguage('en') // 源语言,默认英语: en
+    -> setTargetLanguage('zh-CN') // 翻译目标语言, 默认中文简体 : zh-CN
+    -> setProxy('http://127.0.0.1:1081') // 配置http代理
     -> translate();
 
 var_dump($result);
